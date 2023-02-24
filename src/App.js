8294b8863c03fa1661c92816f0304cb775907d2e
Jamie Ohax <jimohax@gmail.com>
2023-02-24 12:45:25 +0100

    nav done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-// import './index.css'
+import './App.css'
+
+import Navbar from "./Components/Navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
-      i am here with new settings
-      <h1 className="change">this should work fully again</h1>
-      <p>This should also work</p>
+      <Navbar />
+            
     </div>
   );
 }
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import './Navbar.css'
+
+const Navbar = () => {
+  return (
+    <div className="nav-wrap">
+        <div className="nav-left">
+            <div className="nav-logo">Jamie</div>
+            <span>Toggle</span>
+        </div>
+        <div className="nav-right">
+            <div className="nav-list">
+                <ul style={{listStyleType: "none"}}>
+                    <li>Home</li>
+                    <li>Services</li>
+                    <li>Experience</li>
+                    <li>Portflio</li>
+                    <li>Testimonials</li>
+                </ul>
+            </div>
+            <button className="button n-button">Contact Us</button>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
