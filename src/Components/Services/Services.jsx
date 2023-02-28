38aa5bdf9dc15d08abb152fad12b1738fd5a3f7d
Jamie Ohax <jimohax@gmail.com>
2023-02-28 03:20:55 +0100

    fixed skill card

--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -9,35 +9,35 @@ const Services = () => {
   return (
     <div className="Section2">
         <div className="Sec2-left">
-            <div className="Sec2Title">
-                <span>My Awesome</span>
-                <span>Services</span>
-                <span>This is just a glypse of my super powers. There is more i can do, as i go above and beyond just to  give my clients the very best with touching edge technology</span>
-            </div>
-            <button className="button section2button">Download CV</button>
-            <div
-					className="blur"
-					style={{ background: "#ABF1FF94" }}
-				></div>
+              <div className="Sec2Title">
+                  <span>My Awesome</span>
+                  <span>Services</span>
+                  <span>This is just a glypse of my super powers. There is more i can do, as i go above and beyond just to  give my clients the very best with touching edge technology</span>
+              </div>
+              <button className="button section2button">Download CV</button>
+              <div
+            className="blur"
+            style={{ background: "#ABF1FF94" }}
+            ></div>
         </div>
         <div className="Sec2-right">
-          <div className="card">
-            <SkillCard style={{top: '12rem', left: '-4rem'}}
-            txt1="Design" 
-            txt2="Photoshop, Figma"
-            emoji={heart}
-            />
-            <SkillCard
-            txt1="Frontend" 
-            txt2="HTML, CSS, Javascript, React, Bootstrap"
-            emoji={glasses}
-            />
-            <SkillCard
-            txt1="Backend" 
-            txt2="NodeJs, ExpressJs, MongoDB, Redux"
-            emoji={humble}
-            />
-          </div>
+            <div className="card">
+              <SkillCard 
+              txt1="Design" 
+              txt2="Photoshop, Figma"
+              emoji={heart}
+              />
+              <SkillCard 
+              txt1="Frontend" 
+              txt2="HTML, CSS, Javascript, React, Bootstrap"
+              emoji={glasses}
+              />
+              <SkillCard 
+              txt1="Backend" 
+              txt2="NodeJs, ExpressJs, MongoDB, Redux"
+              emoji={humble}
+              />
+            </div>
         </div>
     </div>
   )
